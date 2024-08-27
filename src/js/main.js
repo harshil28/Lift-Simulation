a0870534d0302345ebd580b5b2e927652c5a5aa8
Harshil <harshildilse2000@gmail.com>
2024-08-27 10:31:36 +0530

    WIP : changes in css to get floor ready

--- src/js/main.js
+++ src/js/main.js
@@ -2,9 +2,22 @@ let floorInput = document.querySelector(`#floorCount`);
 let liftInput = document.querySelector(`#liftCount`);
 let button = document.querySelector(`#generateButton`);
 
+let container = document.querySelector(`.container`);
+
 function clickHandler() {
-  console.log(floorInput.value);
-  console.log(liftInput.value);
+  let floorValue = floorInput.value;
+let liftValue = liftInput.value;
+  if(floorValue <= 1 ){
+    alert("Number of floors should be greater than 1 !!");
+  }if(liftValue < 1){
+    alert("Number of lifts should be POSITIVE !!");
+  }
+}
+
+function showErrorMessage(message) {
+  container.style.display = "block";
+  container.style.color = "red";
+  container.innerText = message;
 }
 
 button.addEventListener("click", clickHandler);
