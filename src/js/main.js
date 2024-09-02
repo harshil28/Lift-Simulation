b1efdead34b7087d129b92e2e0b98523e0e96f9a
Harshil <harshildilse2000@gmail.com>
2024-09-02 14:33:03 +0530

    refactor: made UI dynamic and lift doors open open on hover (Need to work on responsiveness)

--- src/js/main.js
+++ src/js/main.js
@@ -1,23 +1,92 @@
 let floorInput = document.querySelector(`#floorCount`);
 let liftInput = document.querySelector(`#liftCount`);
-let button = document.querySelector(`#generateButton`);
+let generateButton = document.querySelector(`#generateButton`);
+let clearButton = document.querySelector(`#clearButton`);
+let container = document.querySelector(`#mainContainer`);
+let finalhr = document.querySelector(`#final_hr`);
 
-let container = document.querySelector(`.container`);
-
-function clickHandler() {
-  let floorValue = floorInput.value;
-let liftValue = liftInput.value;
-  if(floorValue <= 1 ){
-    alert("Number of floors should be greater than 1 !!");
-  }if(liftValue < 1){
-    alert("Number of lifts should be POSITIVE !!");
+function divContainer(numOfFloors) {
+  container.innerHTML = "";
+  for (let i = 0; i < numOfFloors; i++) {
+    newFloor(i);
   }
 }
 
-function showErrorMessage(message) {
-  container.style.display = "block";
-  container.style.color = "red";
-  container.innerText = message;
+function newFloor(floor) {
+  let userFloorValue = floorInput.value;
+  let floorNo = userFloorValue - floor;
+  const newDiv1 = document.createElement("div");
+  newDiv1.className = `floorValaDiv`;
+
+  const floorNameDiv = document.createElement("div");
+  floorNameDiv.className = `floorName`;
+  floorNameDiv.id = `floorName` + floorNo;
+  floorNameDiv.innerHTML = floorNo;
+  newDiv1.appendChild(floorNameDiv);
+
+  const buttonDiv = document.createElement("div");
+  buttonDiv.className = `liftButtonDiv`;
+
+  if (floor >= 1) {
+    const upLiftButtonDiv = document.createElement("div");
+    upLiftButtonDiv.innerHTML = "U";
+    upLiftButtonDiv.className = `liftButton`;
+    buttonDiv.appendChild(upLiftButtonDiv);
+  }
+
+  if (floor < userFloorValue - 1) {
+    const downLiftButtonDiv = document.createElement("div");
+    downLiftButtonDiv.innerHTML = "D";
+    downLiftButtonDiv.className = `liftButton`;
+    buttonDiv.appendChild(downLiftButtonDiv);
+  }
+
+  newDiv1.appendChild(buttonDiv);
+
+  if (floor == userFloorValue - 1) {
+    for (let j = 0; j < liftInput.value; j++) {
+      newDiv1.appendChild(nayaLift());
+    }
+  }
+
+  //newDiv1.appendChild(liftDiv);
+  container.appendChild(newDiv1);
+  container.appendChild(document.createElement("hr"));
 }
 
-button.addEventListener("click", clickHandler);
+function nayaLift() {
+  const liftDiv = document.createElement("div");
+  liftDiv.className = `lift`;
+
+  const leftDoorDiv = document.createElement("div");
+  leftDoorDiv.className = `liftDoorLeft`;
+
+  const rightDoorDiv = document.createElement("div");
+  rightDoorDiv.className = `liftDoorRight`;
+
+  liftDiv.appendChild(leftDoorDiv);
+  liftDiv.appendChild(rightDoorDiv);
+  return liftDiv;
+}
+
+function clickHandler() {
+  let floorValue = floorInput.value;
+  let liftValue = liftInput.value;
+  if (floorValue <= 1) {
+    return alert("Number of floors should be greater than 1 !!");
+  }
+  if (liftValue < 1) {
+    return alert("Number of lifts should be 1 or more than 1 !!");
+  }
+  // createDiv(floorValue);
+  divContainer(floorValue);
+}
+
+function clearAll() {
+  container.innerHTML = "";
+  floorInput.value = "";
+  liftInput.value = "";
+}
+
+generateButton.addEventListener("click", clickHandler);
+clearButton.addEventListener("click", clearAll);
